9fff0a6be98a5c2b72b8dc814edafa3cb46ce535
pstolinski <pstolinski2@huskers.unl.edu>
2026-09-14 09:21:33 -0500

    Finishing lab with last part

--- include/stepper.h
+++ include/stepper.h
@@ -5,7 +5,7 @@
 
 #define STEP_PIN 4
 #define DIR_PIN 5
-#define STEP_DELAY 1000 // us, min is 1us per A4988 datasheet
+#define STEP_DELAY 2500
 
 void stepperInit();
 void stepperCW(int steps);
--- src/main.cpp
+++ src/main.cpp
@@ -6,5 +6,26 @@ void setup() {
 }
 
 void loop() {
-  stepperCCW(200);
+  //stepperCW(200);
+  //stepperCCW(200);
+
+  //increases step speed conservatively
+  for(int delayTime = 2500; delayTime >= 250; delayTime -= 250) {
+    digitalWrite(DIR_PIN, HIGH);
+    for (int i = 0; i < 200; i++) {
+      digitalWrite(STEP_PIN, HIGH);
+      delayMicroseconds(delayTime);
+      digitalWrite(STEP_PIN, LOW);
+      delayMicroseconds(delayTime);
+    }
+    delay(500); 
+    digitalWrite(DIR_PIN, LOW);
+    for (int i = 0; i < 200; i++) {
+      digitalWrite(STEP_PIN, HIGH);
+      delayMicroseconds(delayTime);
+      digitalWrite(STEP_PIN, LOW);
+      delayMicroseconds(delayTime);
+    }
+    delay(500); 
+  }
 }
